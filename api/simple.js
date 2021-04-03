dc9b32dd13253e772a21919e2d57a67590a818f6
tiencoffee <tiencoffee4@gmail.com>
2021-04-03 15:01:33 +0700

    bắt lỗi và trả về trong api/simple

--- api/simple.js
+++ api/simple.js
@@ -2,13 +2,19 @@ const ytdl = require("ytdl-core")
 
 module.exports = async (req, res) => {
 	const {id, quality, filter, format} = req.query
-	let data = await ytdl.getInfo(id)
-	let details = data.videoDetails
-	delete details.availableCountries
-	let formats = ytdl.chooseFormat(data.formats, {quality, filter, format})
-	data = {
-		details: data.videoDetails,
-		formats: formats
+	try {
+		let data = await ytdl.getInfo(id)
+		let details = data.videoDetails
+		delete details.availableCountries
+		let formats = ytdl.chooseFormat(data.formats, {quality, filter, format})
+		data = {
+			details: data.videoDetails,
+			formats: formats
+		}
+		res.json(data)
+	} catch (err) {
+		res.status(400).json({
+			err: err.message
+		})
 	}
-	res.json(data)
 }
