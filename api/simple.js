5e7f26f2b141fda40965514051d28b9adf403e66
tiencoffee <tiencoffee4@gmail.com>
2021-04-03 14:28:48 +0700

    delete details.availableCountries

--- api/simple.js
+++ api/simple.js
@@ -3,11 +3,12 @@ const ytdl = require("ytdl-core")
 module.exports = async (req, res) => {
 	const {id, quality, filter, format} = req.query
 	let data = await ytdl.getInfo(id)
+	let details = data.videoDetails
+	delete details.availableCountries
 	let formats = ytdl.chooseFormat(data.formats, {quality, filter, formats})
 	data = {
 		details: data.videoDetails,
 		formats: formats
 	}
-	data = JSON.stringify(data)
 	res.json(data)
 }
